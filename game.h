31530a8cc101961d81a28404bdc12ebf610735e4
Gage Gailbreath <gsgailbreath@gmail.com>
2023-10-21 00:39:16 -0400

    Added class structure with initial values

--- game.h
+++ game.h
@@ -14,6 +14,7 @@
 #define STRAW   5
 #define GOBLIN   2
 #define SCIENTIST   1
+#define NUM_CLASSES 6
 
 // Equipment Classes
 #define SPECIAL 0
@@ -31,23 +32,25 @@ typedef struct{
     uint8_t last;
 } message_pramble;
 
+typedef union{
+    uint8_t stat[16];
+    struct{
+        uint8_t speed : 4;
+        uint8_t view : 4;
+        uint8_t health;
+        uint16_t gold;
+        uint8_t armor;
+        uint8_t attack;
+        uint8_t special;
+        uint8_t reserved[9];
+    };
+}stat_block;
+
 typedef struct{
     uint8_t uid[7];
     message_pramble preamble;
     uint8_t text_line[8][14];
-    union{
-        uint8_t stats[16];
-        struct{
-            uint8_t speed;
-            uint8_t health;
-            uint16_t gold;
-            uint8_t armor;
-            uint8_t attack;
-            uint8_t view;
-            uint8_t special;
-            uint8_t reserved[8];
-        };
-    };
+    stat_block stats;
     uint8_t reserved[28];
     union{
         struct{
@@ -61,5 +64,13 @@ typedef struct{
     };
 } NTAG215;
 
+typedef struct{
+    uint8_t *title;
+    uint8_t *body;
+    stat_block stats;
+    uint8_t portrait;
+} class_entry;
+
+extern class_entry classes[NUM_CLASSES];
 
 #endif
